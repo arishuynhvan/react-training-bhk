0fdf76ad97fe0d36890b90f4734f8a50c1c97b78
arishuynhvan <arishuynhvan@gmail.com>
2020-06-10 16:48:56 +0700

    Navigate to Meal Detail Screen

--- elearning/App.js
+++ elearning/App.js
@@ -8,7 +8,6 @@
 import React from 'react';
 import {
   StyleSheet,
-  View,
 } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
