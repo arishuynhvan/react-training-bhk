9875de21eaf1935201ec1fdde316ff15ba3615ef
arishuynhvan <arishuynhvan@gmail.com>
2020-09-14 18:05:01 +0700

    Save img file with FileSystem

--- devicefeature/store/places-action.js
+++ devicefeature/store/places-action.js
@@ -1,4 +1,19 @@
+import * as FileSystem from "expo-file-system";
 export const ADD_PLACE = "ADD_PLACE";
 export const addPlace = (title, imgPath) => {
-  return { type: ADD_PLACE, placeData: { title: title, image: imgPath } };
+  return async (dispatch) => {
+    const fileName = imagePath.split("/").pop(); //get the image file name - last element of the imagePath split by /
+    const newPath = FileSystem.documentDirectory + fileName;
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  };
+
+  dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
 };
